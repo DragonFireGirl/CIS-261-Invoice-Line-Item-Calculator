1c2444c116d092977f4dd2c33e03b03423fcd3da
DragonFireGirl <bshaelyn320@gmail.com>
2023-02-07 10:25:35 -0800

    cool

--- CIS_261_Invoice_Line_Item_Calculator.py
+++ CIS_261_Invoice_Line_Item_Calculator.py
@@ -1,1 +1,47 @@
+#Shaelyn Britney
+#CIS 261
+#Invoice Line Item Calculator
 
+#!/usr/bin/env python3
+
+def get_price():
+    while True:
+        try:
+            price = float(input("Enter price: "))
+            return price
+        except ValueError:
+            print("Invalid decimal number. Please try again.")
+
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid integer. Please try again.")
+
+def main():
+    print("The Invoice Line Item Calculator\n")
+
+    answer = "y"
+    while answer.lower() == "y":
+        # get the price and quantity
+        price = get_price()
+        quantity = get_quantity()
+    
+        # calculate the total
+        total = price * quantity
+
+        # display the results
+        print()
+        print("PRICE:    ", f"{price: .2f}")
+        print("QUANTITY: ", quantity)
+        print("TOTAL:    ", f"{total: .2f}")
+        answer = input("Enter another line item? (y/n): ")
+        print()
+        
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
